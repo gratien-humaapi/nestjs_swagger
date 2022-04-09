0f1ff435f54e47f297e0fc9c30d4284f0bb70368
pyvvo <kenneth.ag@humaapi.com>
2022-04-09 16:12:19 +0200

    test2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { CatsServiceService } from './cats-service/cats-service.service';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, CatsServiceService, CatsService]
 })
 export class AppModule {}
