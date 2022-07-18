36317aa32eb6445947de5735a4d6190a5b52964d
pyvvo <kenneth.ag@humaapi.com>
2022-07-18 16:25:48 +0100

    feat(modules): main helmet add
    helmet Set sensible headers for improved security

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(cookieParser());
+  app.use(cookieParser()); // Parse the `/token` refresh cookie
+  app.use(helmet()); // Set sensible headers for improved security
   await app.listen(3001);
 }
 bootstrap();
