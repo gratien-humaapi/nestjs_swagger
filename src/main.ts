f27d34892431c5f2d03b897d7b2f31608cd09556
pyvvo <kenneth.ag@humaapi.com>
2022-07-19 13:54:24 +0100

    fix(modules): main helmet CSP
    GraphQL CSP error

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser()); // Parse the `/token` refresh cookie
-  app.use(helmet()); // Set sensible headers for improved security
+  /**
+   * @see https://docs.nestjs.com/security/helmet#use-with-fastify
+   * @see https://github.com/graphql/graphql-playground/issues/1283#issuecomment-723686116
+   */
+  app.use(
+    helmet({
+      contentSecurityPolicy: process.env.NODE_ENV === "dev" ? false : undefined,
+      crossOriginEmbedderPolicy:
+        process.env.NODE_ENV === "dev" ? false : undefined
+    })
+  ); // Set sensible headers for improved security
   await app.listen(3001);
 }
 bootstrap();
