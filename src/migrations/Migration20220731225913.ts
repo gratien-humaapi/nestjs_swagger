f3e686a96bf390bbd4715b55cd14baa115cd3ade
pyvvo <kenneth.ag@humaapi.com>
2022-09-13 12:11:42 +0100

    chore(migrations): stop migration creation for dev env
    we use mikro-orm schema:fresh --run for an instant database migration

--- src/migrations/Migration20220731225913.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from "@mikro-orm/migrations";
-
-export class Migration20220731225913 extends Migration {
-  async up(): Promise<void> {
-    this.addSql(
-      'create table "student" ("id" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "email" varchar(255) not null, "age" int null, constraint "student_pkey" primary key ("id"));'
-    );
-  }
-}
